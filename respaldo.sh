93e1e2a212dac89d249221b84a3cb530320f67f5
matthew-hss <matthew.hss@hotmail.com>
2017-03-24 13:34:08 -0300

    script respaldo

---
+++ respaldo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+directorio="/tmp/repo"
+if [ -d $directorio ] ; then
+	directorio="/tmp/repo"
+else
+	echo `mkdir $directorio`
+fi
+
+echo `git bundle create repo.bundle --all`
+
+echo `mv repo.bundle $directorio`
+
+echo `mysqldump -u root -pa+b=c 5f197c7b6ede3ff0 > dbdump.sql`
+
+echo `mv dbdump.sql $directorio`
+
+echo `tar -cJf repo`date +%Y%m%d%H%M`.tar.xz $directorio`
+
+echo `rm -fr $directorio`
+

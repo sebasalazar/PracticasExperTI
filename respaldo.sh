c8c0dc4a734f96dd43772f410a661d866ebe749e
matthew-hss <matthew.hss@hotmail.com>
2017-03-28 15:37:02 -0300

    Update respaldo.sh

--- respaldo.sh
+++ respaldo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 directorio="/tmp/repo"
+date=`date +%Y%m%d%H%M`
 if [ -d $directorio ] ; then
-	directorio="/tmp/repo"
+        directorio="/tmp/repo"
 else
-	echo `mkdir $directorio`
+        echo `mkdir $directorio`
 fi
 
 echo `git bundle create repo.bundle --all`
@@ -14,7 +15,6 @@ echo `mysqldump -u root -pa+b=c 5f197c7b6ede3ff0 > dbdump.sql`
 
 echo `mv dbdump.sql $directorio`
 
-echo `tar -cJf repo`date +%Y%m%d%H%M`.tar.xz $directorio`
+echo `tar -cJf repo$date.tar.xz $directorio`
 
 echo `rm -fr $directorio`
-

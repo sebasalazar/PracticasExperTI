51f9521e30a70302856881748671dd971f74605b
matthew-hss <matthew.hss@hotmail.com>
2017-03-30 12:10:43 -0300

    Update respaldo.sh

--- respaldo.sh
+++ respaldo.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 directorio="/tmp/repo"
 date=`date +%Y%m%d%H%M`
+destino="/tmp/backup"
 if [ -d $directorio ] ; then
         directorio="/tmp/repo"
 else
         mkdir $directorio
+fi
+
+if [ -d $destino ] ; then
+        destino="/tmp/backup"
+else
+        mkdir $destino
 fi
 
 git bundle create repo.bundle --all
@@ -15,6 +22,10 @@ mysqldump -u root -pa+b=c 5f197c7b6ede3ff0 > dbdump.sql
 
 mv dbdump.sql $directorio
 
+cp -r /home/experti/sources/frappe-bench/ $directorio
+
 tar -cJf repo$date.tar.xz $directorio
+
+mv repo$date.tar.xz $destino
 
 rm -fr $directorio
